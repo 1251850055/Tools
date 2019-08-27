2b96530b57b8b316c1d87209d4280c258333dbd8
wangzehui <wangzehui@oristartech.com>
2019-08-27 21:29:36 +0800

    修改

--- src/main/java/com/wzh/tools/utils/check/ListUtil.java
+++ src/main/java/com/wzh/tools/utils/check/ListUtil.java
@@ -19,21 +19,27 @@ public class ListUtil {
      * @return
      */
     public static boolean isListEqual(List l0, List l1) {
-        if (l0 == l1)
+        if (l0 == l1) {
             return true;
-        if (l0 == null && l1 == null)
+        }
+        if (l0 == null && l1 == null) {
             return true;
-        if (l0 == null || l1 == null)
+        }
+        if (l0 == null || l1 == null) {
             return false;
-        if (l0.size() != l1.size())
+        }
+        if (l0.size() != l1.size()) {
             return false;
+        }
         for (Object o : l0) {
-            if (!l1.contains(o))
+            if (!l1.contains(o)) {
                 return false;
+            }
         }
         for (Object o : l1) {
-            if (!l0.contains(o))
+            if (!l0.contains(o)) {
                 return false;
+            }
         }
         return true;
     }
