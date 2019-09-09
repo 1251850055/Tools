7e53ebe3a014f499a8d2c16652292ebceaa72164
wangzehui <wangzehui@oristartech.com>
2019-09-09 15:21:16 +0800

    查询用户列表

--- src/main/java/com/wzh/tools/sql/user.sql
+++ src/main/java/com/wzh/tools/sql/user.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 27/08/2019 20:33:40
+ Date: 09/09/2019 15:20:36
 */
 
 SET NAMES utf8mb4;
@@ -23,17 +23,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '姓名',
   `user_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户编码',
-  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `user_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
   `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `update_time` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '更新时间，自动修改',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, '王泽辉', '20', '18500507445', '2019-08-27 19:59:06', '2019-08-27 19:59:08');
+INSERT INTO `user` VALUES (2, '王泽辉2', '21', '18500507446', '2019-08-27 19:59:06', '2019-08-30 19:59:08');
+INSERT INTO `user` VALUES (3, '王泽辉2', '22', '18500507446', '2019-08-27 19:59:06', '2019-08-30 19:59:08');
 
 SET FOREIGN_KEY_CHECKS = 1;
