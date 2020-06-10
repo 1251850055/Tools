dd6adc3e69ae7f6f730b29eac5b255bbb37808e3
wangzehui <1251850055@qq.com>
2020-06-10 15:13:52 +0800

    kbt-update

---
+++ src/test/java/com/wzh/tools/DataA.java
@@ -0,0 +1,20 @@
+package com.wzh.tools;
+
+/**
+ * @Description:
+ * @Author: wangzehui
+ * @Date: 2020/6/8 16:53
+ */
+
+public class DataA {
+
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
--- src/test/java/com/wzh/tools/Test.java
+++ src/test/java/com/wzh/tools/Test.java
@@ -1,5 +1,7 @@
 package com.wzh.tools;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * @Description:
  * @Author: wangzehui
@@ -8,8 +10,16 @@ package com.wzh.tools;
 
 public class Test {
     public static void main(String[] args) {
-        long faceId = 797894127L;
-        System.out.println(Long.toBinaryString(faceId));
+        String[] arr = {"1", "2", "3", "4"};
+        StringBuilder a = new StringBuilder();
+        for (String s : arr) {
+            a.append(",").append(s);
+        }
+        a = new StringBuilder(a.substring(1, a.length()));
+        DataA data1 = new DataA();
+        data1.setData(a.toString());
+        String s = JSONObject.toJSONString(data1);
+        System.out.println(s);
 
     }
 }
