780ec0555a4b2173f8169f005f4093e5ddec2592
wangzehui <1251850055@qq.com>
2020-08-26 14:51:55 +0800

    上传测试

--- src/test/java/com/wzh/tools/Test.java
+++ src/test/java/com/wzh/tools/Test.java
@@ -1,9 +1,6 @@
 package com.wzh.tools;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.wzh.tools.utils.time.StringUtil;
 
 /**
  * @Description:
@@ -13,25 +10,40 @@ import java.util.Date;
 
 public class Test {
 
-    public static final String pattern = "M-dd";
+    public static final String[] character = {"校长15", "学校管理员10", "年级长13", "学科组长20", "班主任12", "学科教师11"};
 
     public static void main(String[] args) {
-
-        try {
-            Date date = new Date();
-            DateFormat df1 = DateFormat.getDateInstance();//日期格式，精确到日
-            System.out.println(df1.format(date));
-
-            SimpleDateFormat df = new SimpleDateFormat(pattern);
-            String format = df.format(date);
-
-            Date parse = df.parse(df1.format(date));
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
+//        String maxRole = getMaxRole("学校管理员10,年级长13,学科教师11,校长15");
+        String maxRole1 = getMaxRole("10,年级长13,学科教师11,15");
+//        System.out.println(maxRole);
+        System.out.println(maxRole1);
     }
 
+
+    public static String getMaxRole(String roleStr) {
+        String[] split = roleStr.split(",");
+        StringBuilder sb = new StringBuilder();
+        String str = "";
+        if (null != split && split.length > 0) {
+            for (String value : character) {
+                for (String s1 : split) {
+                    if (s1.equals(value)) {
+                        sb.append(value).append(",");
+                        break;
+                    }
+                }
+            }
+            if (StringUtil.isNotEmpty(sb.toString())) {
+                String[] split1 = sb.toString().split(",");
+                if (null != split1 && split1.length > 0) {
+                    if (StringUtil.isNotEmpty(split1[0])) {
+                        str = split1[0];
+                    }
+
+                }
+            }
+        }
+        return str;
+    }
 
 }
