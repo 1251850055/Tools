ca0773b7a105e7af7486da7e22d6539eb8e673b6
wangzehui <1251850055@qq.com>
2020-11-09 15:43:52 +0800

    骑士

--- src/test/java/com/wzh/tools/Test.java
+++ src/test/java/com/wzh/tools/Test.java
@@ -1,7 +1,5 @@
 package com.wzh.tools;
 
-import com.alibaba.fastjson.JSONObject;
-
 /**
  * @Description:
  * @Author: wangzehui
@@ -11,14 +9,6 @@ import com.alibaba.fastjson.JSONObject;
 public class Test {
 
     public static void main(String[] args) {
-        String s = "{\"bookId\":437,\"courseId\":\"02\",\"errorNum\":1,\"studentId\":386,\"type\":2}";
-
-        JSONObject jsonObject = JSONObject.parseObject(s);
-
-
-        String bookId = jsonObject.get("bookId").toString();
-
-        System.out.println(bookId);
 
 
     }
