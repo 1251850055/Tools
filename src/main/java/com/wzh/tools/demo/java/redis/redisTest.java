1ce7778c5b98d81f011d7c3adcdc6cac78cf47e7
wangzehui <1251850055@qq.com>
2021-04-17 14:31:14 +0800

    RedisConfig

--- src/main/java/com/wzh/tools/demo/java/redis/redisTest.java
+++ src/main/java/com/wzh/tools/demo/java/redis/redisTest.java
@@ -1,5 +1,7 @@
 package com.wzh.tools.demo.java.redis;
 
+import redis.clients.jedis.Jedis;
+
 /**
  * @Description:
  * @Author: wangzehui
@@ -7,7 +9,21 @@ package com.wzh.tools.demo.java.redis;
  */
 
 public class redisTest {
+
+    /**
+     * 描述 redis 地址：https://github.com/microsoftarchive/redis/releases
+     * 本地运行 redis-server.exe启动redis服务器
+     * redis客户端 redis-cli.exe
+     */
     public static void main(String[] args) {
+        //1.链接redis
+        Jedis jedis = new Jedis("localhost", 6379);
+        //2.操作redis
+        jedis.set("name", "wzh");
+        //3.取出值
+        System.out.println(jedis.get("name"));
+        //4.关闭链接
+        jedis.close();
 
     }
 
